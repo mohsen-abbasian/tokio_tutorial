ae4f447e37d75672470a9b5c88246b739f2b8e26
Mohsen <moe.abbasian@gmail.com>
2024-09-04 23:12:13 +0330

    add sharden db

--- src/shared_state_mod.rs
+++ src/shared_state_mod.rs
@@ -4,6 +4,18 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
+
+fn new_sharded_db(num_shards: usize) -> ShardedDb {
+    let mut db = Vec::with_capacity(num_shards);
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(db)
+}
+
+// let shard = db[hash(key) % db.len()].lock().unwrap();
+// shard.insert(key, value);
 
 pub async fn main_shared_state() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();

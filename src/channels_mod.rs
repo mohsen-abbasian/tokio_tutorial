7ffb6e484200425246b8ec8204bb574a94da4670
moe <moe.abbasian@gmail.com>
2024-09-05 13:52:03 +0330

    add manager task and establish a channel to establish communication

--- src/channels_mod.rs
+++ src/channels_mod.rs
@@ -1,22 +1,58 @@
 use bytes::Bytes;
 use mini_redis::client;
+use tokio::sync::mpsc;
 
-// pub async fn main_channel() {
-//     // Stablish a connection to server
-//     let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+pub async fn main_channel() {
+    // Create a new channel with a capacity of at most 32.
+    let (tx, mut rx) = mpsc::channel(32);
 
-//     // Spawn two tasks, one gets a key, the other sets a key
-//     let t1 = tokio::spawn(async {
-//         let res = client.get("foo").await;
-//     });
+    // The `move` keyword is used to **move** ownership of `rx` into the task.
+    let manager = tokio::spawn(async move {
+        // Establish a connection to the server
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
 
-//     let t2 = tokio::spawn(async {
-//         client.set("foo", "bar".into()).await;
-//     });
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
 
-//     t1.await.unwrap();
-//     t2.await.unwrap();
-// }
+            match cmd {
+                Get {key} => {
+                    client.get(&key).await;
+                }
+                Set {key, val }=> {
+                    client.set(&key, val).await;
+                }
+            }
+        }
+    });
+
+    // The `Sender` handles are moved into the tasks. As there are two
+    // tasks, we need a second `Sender`.
+    let tx2 = tx.clone();
+
+    // Spawn two tasks, one gets a key, the other sets a key
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string()
+        };
+
+        tx.send(cmd).await.unwrap();
+    });
+
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+
+        tx2.send(cmd).await.unwrap();
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+
+}
 
 #[derive(Debug)]
 enum Command {

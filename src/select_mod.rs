a380836e583b60d730a6879445d2608280d72569
moe <moe.abbasian@gmail.com>
2024-09-07 13:24:33 +0330

    add sleep to spawned tasks in main_select

--- src/select_mod.rs
+++ src/select_mod.rs
@@ -1,15 +1,18 @@
 // Implementation of the eighth section of tutorial (Select)
 use tokio::sync::oneshot;
+use tokio::time::{Duration, sleep};
 
 pub async fn main_select() {
     let (tx1, rx1) = oneshot::channel();
     let (tx2, rx2) = oneshot::channel();
 
     tokio::spawn(async {
+        sleep(Duration::from_secs(2)).await;
         let _ = tx1.send("one");
     });
 
     tokio::spawn(async {
+        sleep(Duration::from_secs(1)).await;
         let _ = tx2.send("two");
     });
 
